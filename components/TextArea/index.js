d53c74b58cc3ba0eca61da420ace308009518c2f
Oscar Andersson <anderssonoscar0@gmail.com>
2021-07-08 19:04:12 +0200

    formatting

--- components/TextArea/index.js
+++ components/TextArea/index.js
@@ -1,17 +1,16 @@
-
-
-
 export default function TextArea(props) {
   return (
     <div className="input-group">
       <span className="input-group-text">Input</span>
       <textarea
-      className="form-control"
-      aria-label="With textarea"
-      onChange={(e) => {
-        props.onChangeFunction(e.target.value);
-      }}
-      rows="5"
-      autoFocus/>
-      </div>)
+        className="form-control"
+        aria-label="With textarea"
+        onChange={(e) => {
+          props.onChangeFunction(e.target.value);
+        }}
+        rows="5"
+        autoFocus
+      />
+    </div>
+  );
 }
