4ed87d1921552f47b69c50e125d22b30ca025929
Oscar Andersson <anderssonoscar0@gmail.com>
2021-07-13 00:44:01 +0200

    #6 add chakra

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,12 +1,14 @@
-export default function TextArea(props) {
+import { Button } from '@chakra-ui/react';
+
+export default function ButtonB(props) {
   return (
-    <button
+    <Button
       type="button"
       className={`btn btn-lg btn-${props.buttonType || 'primary'}`}
       disabled={props.disabled}
       onClick={props.onClickFunction}
     >
       {props.title}
-    </button>
+    </Button>
   );
 }
