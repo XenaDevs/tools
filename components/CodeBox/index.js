24fa50079428922abe36d091a2ac9084f9df808b
Oscar Andersson <anderssonoscar0@gmail.com>
2021-07-15 16:24:57 +0200

    center stuff

--- components/CodeBox/index.js
+++ components/CodeBox/index.js
@@ -2,18 +2,26 @@ import { Code } from '@chakra-ui/react';
 
 const CodeBox = (props) => {
   return (
-    <Code
-      colorScheme={props.colorScheme || 'gray'}
+    <div
       style={{
         display: 'flex',
         flexDirection: 'column',
-        textAlign: 'center',
-        width: props.width || '25%',
-        borderRadius: 3,
+        alignItems: 'center',
       }}
     >
-      {props.code}
-    </Code>
+      <Code
+        colorScheme={props.colorScheme || 'gray'}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+          width: props.width || '25%',
+          borderRadius: 3,
+        }}
+      >
+        {props.code}
+      </Code>
+    </div>
   );
 };
 
