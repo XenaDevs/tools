73dc6e909531e3d61cd18321a339489ff55dc124
Oscar Andersson <anderssonoscar0@gmail.com>
2021-08-30 13:57:36 +0200

    new fix

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  presets: ['@babel/preset-react'],
+  presets: [
+    '@babel/preset-react',
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+  ],
   plugins: ['require-context-hook'],
 };
