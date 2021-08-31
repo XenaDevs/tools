4e44a8fee8fad9bf96ef1326686485df2dcc4452
Oscar Andersson <anderssonoscar0@gmail.com>
2021-08-31 14:49:09 +0200

    fix #28

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Center } from '@chakra-ui/react';
+import { Center, Link as ChakraLink } from '@chakra-ui/react';
 import Link from 'next/link';
 const Header = () => {
   return (
     <Center h="50px" boxShadow="1px 1px 1px #ccc" fontSize="1.8rem" mb="4">
-      <Link href="/">Tools</Link>
+      <ChakraLink>
+        <Link href="/">Tools</Link>
+      </ChakraLink>
     </Center>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Heading, Box } from '@chakra-ui/react';
+import { Text, Heading, Box, Link as ChakraLink } from '@chakra-ui/react';
 import Link from 'next/link';
 import { BUILD_TOOLS_LIST } from '../utils/utils';
 
@@ -7,9 +7,12 @@ const Home = () => {
   const tools = BUILD_TOOLS_LIST().map((tool) => (
     <>
       <Box my="4">
-        <Link href={tool.path} passHref>
-          <Heading size="lg">{tool.label}</Heading>
-        </Link>
+        <ChakraLink>
+          <Link href={tool.path} passHref>
+            <Heading size="lg">{tool.label}</Heading>
+          </Link>
+        </ChakraLink>
+
         <Text>{tool.shortDesc}</Text>
       </Box>
     </>
