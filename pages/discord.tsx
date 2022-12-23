7ea371e4f871140478cb3517437ce7835c147b9c
Oscar Andersson <anderssonoscar0@gmail.com>
2022-12-23 22:24:44 +0100

    feat: add discord

---
+++ pages/discord.tsx
@@ -0,0 +1,14 @@
+export async function getStaticProps() {
+  return {
+    redirect: {
+      destination: "https://discord.gg/fJpypWzSyH",
+      permanent: false,
+    },
+  };
+}
+
+const Discord = () => {
+  return <></>;
+};
+
+export default Discord;
