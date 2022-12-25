de1be8ce5c16aa9749a825528efe9d2b2af736a0
Oscar Andersson <anderssonoscar0@gmail.com>
2022-12-25 17:28:25 +0100

    fix: remove old config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ["tsx", "jsx", "js"],
-  async redirects() {
-    return [
-      {
-        source: "/discord",
-        destination: "https://discord.gg/fJpypWzSyH",
-        permanent: false,
-      },
-    ];
-  },
 };
