be53deeef6ef26121dd98b8dc6ccb580648e453d
Oscar Andersson <anderssonoscar0@gmail.com>
2022-12-25 17:23:27 +0100

    fix: discord

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Discord = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    router.push("https://discord.gg/fJpypWzSyH");
+  }, [router.isReady]);
+
   return <></>;
 };
 
 export default Discord;
 
-//https://discord.gg/fJpypWzSyH
+// https://discord.gg/fJpypWzSyH
