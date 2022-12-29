d012963ac58f0c996f62411c8d9881c3dee86d8b
Saveljeff, Jonatan <SaveljeffJonatan@gmail.com>
2022-12-29 11:52:29 +0100

    Ux/UI frontpage (#57)
    * First UI iteration
    
    * shadows
    
    Co-authored-by: Jonatan Saveljeff <jonatan.saveljeff@teleclinic.com>

--- pages/tools/difffinder/tool-metadata.ts
+++ pages/tools/difffinder/tool-metadata.ts
@@ -1,5 +1,5 @@
 export const toolMetaData = {
   path: "/tools/difffinder",
   label: "Diff-finder",
-  shortDesc: "This tool will compare two strings inputs and find show the differences.",
+  shortDesc: "Find string difference.",
 };
