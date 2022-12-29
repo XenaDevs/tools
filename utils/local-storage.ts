c54678cce69c5dfe5ecf6efb815310cf1bbd5300
Jonatan Saveljeff <jonatan.saveljeff@teleclinic.com>
2022-12-29 21:35:16 +0100

    fix misaaaake

--- utils/local-storage.ts
+++ utils/local-storage.ts
@@ -19,7 +19,7 @@ export const getVisitedTools = (): Record<
 export const addToolVisit = (path: string): void => {
   const visitedTools = getVisitedTools();
 
-  if (!visitedTools[path]?.tool.path.includes("/tools/")) return;
+  if (!path.includes("/tools/")) return;
 
   if (visitedTools[path] == null) {
     visitedTools[path] = { visits: 0, tool: toolMap[path] };
