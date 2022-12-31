210e923b7a8ae7da4466851c8dead07de716aad7
Jonatan Saveljeff <jonatan.saveljeff@teleclinic.com>
2022-12-31 16:44:43 +0100

    fix navbar and hotfix topTools

---
+++ pages/_navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="mt-2 flex justify-between mx-auto">
+      <h2 className="text-2xl text-Accent md:text-4xl">Xena</h2>
+      <div className="flex items-center hover:text-CTA hover:underline">
+        <Link href="/contact">Contact us</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
