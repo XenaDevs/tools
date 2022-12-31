1e0d85029f72fda65fcf4fc74d10ae04b2c4bc36
Jonatan Saveljeff <jonatan.saveljeff@teleclinic.com>
2022-12-31 16:22:52 +0100

    fix footer

--- pages/_footer.tsx
+++ pages/_footer.tsx
@@ -2,14 +2,14 @@ import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-auto md:bottom-0 mb-4 mt-4 md:mt-0 flex justify-center px-10 md:px-20 lg:px-40">
-      <p className="text-Accent text-xl">© 2023 Xena-dev</p>
+    <div className="absolute bottom-auto md:bottom-0 pb-4 mt-4 md:mt-0 flex justify-between md:justify-center md:gap-10 px-5 md:px-20 lg:px-40 w-full">
+      <p className="text-Accent text-md">© 2023 Xena-dev</p>
       <div
-        className="flex cursor-pointer ml-4 gap-2"
+        className="flex cursor-pointer ml-4"
         onClick={() => window.open("/discord", "_blank")}
       >
         <BsDiscord fill="#f9a826" className="text-lg md:text-xl" />
-        <h2 className="text-lg text-CTA md:text-xl">Contact us.</h2>
+        <h2 className="text-md text-CTA md:text-xl">Contact us.</h2>
       </div>
     </div>
   );
