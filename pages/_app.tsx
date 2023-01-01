c26b0f8b8eb13ba99ef27cc604b3a4def3d7d3e3
Oscar Andersson <anderssonoscar0@gmail.com>
2023-01-01 21:03:17 +0100

    fix: correct viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {!homePath && <HomeButton />}
       <Component {...pageProps} />
