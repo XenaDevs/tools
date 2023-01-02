5f58cfdc735ccdc15681a8d42b1266e312f72449
Oscar Andersson <anderssonoscar0@gmail.com>
2023-01-02 23:06:54 +0100

    fix: static sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://xena-dev.com",
+  generateRobotsTxt: false,
+  generateIndexSitemap: false,
+  exclude: ["/discord"],
+  autoLastmod: false,
+  changefreq: "monthly",
+};
