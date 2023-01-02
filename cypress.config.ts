8d7406cb87a1209bdb8f7e6fa89c5a1ca1b70a19
Oscar Andersson <anderssonoscar0@gmail.com>
2023-01-02 22:06:05 +0100

    feat: init cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: "__tests__/cypress/*cy.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:3000",
+    supportFile: false,
+  },
+});
