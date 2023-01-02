37dd94054148a9c345793f9ad359fc4725bb0a94
Jonatan Saveljeff <jonatan.saveljeff@teleclinic.com>
2023-01-02 22:44:17 +0100

    contact page + linkbutton

--- pages/_footer.tsx
+++ pages/_footer.tsx
@@ -1,16 +1,14 @@
+import Link from "next/link";
 import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="absolute bottom-auto sm:bottom-0 pb-4 mt-4 md:mt-0 flex justify-between md:justify-center md:gap-10 lg:gap-20 px-5 md:px-20 lg:px-40 w-full">
       <p className="text-Accent text-md">© 2023 Xena-dev</p>
-      <div
-        className="flex cursor-pointer ml-4"
-        onClick={() => window.open("/discord", "_blank")}
-      >
+      <Link className="flex ml-4" href="/discord" target="_blank">
         <BsDiscord fill="#f9a826" className="text-lg md:text-xl" />
         <h2 className="text-md text-CTA md:text-xl">Contact us.</h2>
-      </div>
+      </Link>
     </div>
   );
 };
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,25 +4,31 @@ import {
   AiFillLinkedin,
   AiFillGithub,
 } from "react-icons/ai";
+import { LinkButton } from "../components";
 
 const Contact = () => {
   return (
-    <main className="px-10 md:px-20 lg:px-30 max-w-5xl mx-auto">
+    <main className="px-10 md:px-20 lg:px-30 max-w-7xl mx-auto">
       <section className="min-h-screen pt-10">
-        <h1 className="text-Accent text-2xl mt-5">Reach out to us!</h1>
-        <div className="flex flex-col justify-between">
-          <div className="mt-4">
-            <h2 className="text-xl text-Accent">Jonatan Saveljeff</h2>
-            <h3 className="text-md">Flexible freelancing services</h3>
-            <p className="text-sm mt-3">
+        <div className="flex flex-col mt-5 md:flex-row md:mt-20 md:gap-20 justify-between">
+          <div className="mt-4 w-1/2" id="jonatan">
+            <h2 className="text-xl md:2xl lg:text-3xl text-Accent">
+              Jonatan Saveljeff
+            </h2>
+            <h3 className="text-md md:text-lg lg:text-xl">
+              Flexible freelancing services
+            </h3>
+            <p className="text-sm md:text-md lg:text-lg mt-3">
               I am a freelancer and web developer specializing in creating
               top-quality digital content and improving customer satisfaction
               and conversion rates. Let me help bring your vision to life with
               my skills and passion for technology.
             </p>
+            <p className="my-4 text-Accent text-xl">jonatan@freelance.com</p>
             <div className="flex justify-evenly text-4xl mt-2 text-CTA">
               <AiFillTwitterSquare
                 onClick={() => router.push("https://twitter.com/JonatanSav")}
+                className="hover:cursor-pointer hover:bg-opacity-90"
               />
               <AiFillLinkedin
                 onClick={() =>
@@ -30,30 +36,60 @@ const Contact = () => {
                     "https://www.linkedin.com/in/jonatan-saveljeff-a94650109/"
                   )
                 }
+                className="hover:cursor-pointer hover:bg-opacity-90"
               />
               <AiFillGithub
                 onClick={() =>
                   router.push("https://github.com/Saveljeffjonatan")
                 }
+                className="hover:cursor-pointer hover:bg-opacity-90"
               />
             </div>
           </div>
 
-          <div className="mt-14" />
+          <div className="mt-14 md:hidden" />
+          <div className="w-1 bg-CTA" />
 
-          <h2 className="text-xl text-Accent">Oscar Andersson</h2>
-          <h3 className="text-md">Lorem, ipsum dolor.</h3>
-          <p className="text-sm mt-3">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea,
-            facilis labore cupiditate ut alias sed eveniet illum nisi dolores
-            illo tempora. Libero minus voluptate dolorum totam, laborum voluptas
-            impedit debitis!
-          </p>
-          <div className="flex justify-evenly text-4xl mt-2 text-CTA">
-            <AiFillTwitterSquare />
-            <AiFillLinkedin />
-            <AiFillGithub />
+          <div className="mt-4 w-1/2" id="oscar">
+            <h2 className="text-xl md:text-2xl lg:text-3xl text-Accent">
+              Oscar Andersson
+            </h2>
+            <h3 className="text-md md:text-lg lg:text-xl">
+              Lorem, ipsum dolor.
+            </h3>
+            <p className="text-sm md:text-md lg:text-lg mt-3">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea,
+              facilis labore cupiditate ut alias sed eveniet illum nisi dolores
+              illo tempora. Libero minus voluptate dolorum totam, laborum
+              voluptas impedit debitis!
+            </p>
+            <p className="my-4 text-Accent text-xl">foo@andersson.lazy</p>
+            <div className="flex justify-evenly text-4xl mt-2 text-CTA">
+              <AiFillTwitterSquare
+                className="hover:cursor-pointer hover:bg-opacity-90"
+                onClick={() => router.push("/")}
+              />
+              <AiFillLinkedin
+                className="hover:cursor-pointer hover:bg-opacity-90"
+                onClick={() => router.push("/")}
+              />
+
+              <AiFillGithub
+                onClick={() => router.push("/")}
+                className="hover:cursor-pointer hover:bg-opacity-90"
+              />
+            </div>
           </div>
+        </div>
+
+        <div className="hidden md:flex mt-20" />
+
+        <div className="hidden md:flex flex-col w-fit mx-auto">
+          <h2>Some valuable text here</h2>
+          <p>You what mate?</p>
+          <LinkButton href="/discord" target={"_blank"}>
+            Support Discord
+          </LinkButton>
         </div>
       </section>
     </main>
