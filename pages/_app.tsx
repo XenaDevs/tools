3c0d11728f5e66713512671221c27359f3e14b85
Jonatan Saveljeff <saveljeffjonatan@gmail.com>
2023-01-07 23:44:47 +0100

    Move init GA to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import { addToolVisit } from "../utils/local-storage";
 import { useEffect } from "react";
 import Footer from "./_footer";
 import Head from "next/head";
-import Script from "next/script";
 import { GTMPageView } from "utils/gtm";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -37,20 +36,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <GoogleAnalytics trackPageViews />
       {!homePath && <HomeButton />}
       <Component {...pageProps} />
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W2Z8CBD')
-      `}
-      </Script>
-      <Script
-        id="google_ads"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7374607841609153"
-        crossOrigin="anonymous"
-      />
       <Footer />
     </>
   );
