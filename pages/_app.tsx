4b705b1eda56fb9b26bf6070d1a1000800bca7d8
Saveljeff, Jonatan <SaveljeffJonatan@gmail.com>
2023-01-07 23:51:59 +0100

    Move init GA to document (#97)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import { addToolVisit } from "../utils/local-storage";
 import { useEffect } from "react";
 import Footer from "./_footer";
 import Head from "next/head";
-import Script from "next/script";
 import { GTMPageView } from "utils/gtm";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -42,15 +41,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <GoogleAnalytics trackPageViews />
       {!homePath && <HomeButton />}
       <Component {...pageProps} />
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W2Z8CBD')
-      `}
-      </Script>
       <Footer />
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 // pages/_document.js
 
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default class Document extends NextDocument {
   render() {
     return (
@@ -12,6 +12,15 @@ export default class Document extends NextDocument {
         <body className="font-poppins bg-BG text-White">
           <Main />
           <NextScript />
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W2Z8CBD')
+            `}
+          </Script>
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2Z8CBD" height="0" width="0" style="display: none; visibility: hidden;" />`,
