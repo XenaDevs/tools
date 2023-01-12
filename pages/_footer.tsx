e9451202e2972556c2d809c32758237c8d01678a
Jonatan Saveljeff <saveljeffjonatan@gmail.com>
2023-01-12 10:37:02 +0100

    fix pipe

--- pages/_footer.tsx
+++ pages/_footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Router, useRouter } from "next/router";
+import { useRouter } from "next/router";
 import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
