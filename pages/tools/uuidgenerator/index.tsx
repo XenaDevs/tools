b79e6cdb904e41a06c39eeeb6f5807799cfa1657
Oscar Andersson <anderssonoscar0@gmail.com>
2023-01-21 21:35:05 +0100

    fix: copied text if clicked

--- pages/tools/uuidgenerator/index.tsx
+++ pages/tools/uuidgenerator/index.tsx
@@ -1,58 +1,68 @@
-import { Head } from "components/Head/Head";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { Button } from "../../../components";
+import { Button, ToolTemplate } from "../../../components";
 import { toolMetaData } from "./tool-metadata";
+
+const UuidRow = ({ id }: { id: string }) => {
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <div className="flex gap-10 my-2 items-center justify-center" key={id}>
+      <p className="w-80">{id}</p>
+      <Button
+        key={id}
+        text={copied ? "Copied" : "Copy"}
+        onClick={() => {
+          setCopied(true);
+          navigator.clipboard.writeText(id);
+        }}
+      />
+    </div>
+  );
+};
 
 const UuidGenerator = () => {
   const [amountToGenerate, setAmountToGenerate] = useState(1);
   const [uuids, setUuids] = useState<Array<string>>([]);
 
   useEffect(() => {
-    setUuids([uuidv4()]);
+    setUuids([
+      uuidv4(),
+      uuidv4(),
+      uuidv4(),
+      uuidv4(),
+      uuidv4(),
+      uuidv4(),
+      uuidv4(),
+    ]);
   }, []);
 
-  return (
-    <main className="px-10 md:px-20 lg:px-30 max-w-5xl mx-auto">
-      <Head tool={toolMetaData} />
+  const tool = (
+    <>
+      <div>
+        <input
+          className="text-Black w-20 h-10 mr-5"
+          placeholder="Amount"
+          defaultValue={1}
+          onChange={(e) => setAmountToGenerate(parseInt(e.target.value))}
+        />
 
-      <section className=" py-20">
-        <h1 className="text-xl">UUID V4 generator</h1>
+        <Button
+          text="Generate uuid"
+          onClick={async () => {
+            setUuids([...Array(amountToGenerate)].map(() => uuidv4()));
+          }}
+        />
+      </div>
 
-        <div className="mb-10 mt-5">
-          <input
-            className="text-Black w-20 h-10 mr-5"
-            placeholder="Amount"
-            onChange={(e) => setAmountToGenerate(parseInt(e.target.value))}
-          />
-
-          <Button
-            text="Generate uuid"
-            onClick={async () => {
-              setUuids([...Array(amountToGenerate)].map(() => uuidv4()));
-            }}
-          />
-        </div>
-
-        {uuids.map((id) => {
-          return (
-            <div
-              className="flex gap-10 my-2 items-center justify-center"
-              key={id}
-            >
-              <p>{id}</p>
-              <Button
-                text="Copy"
-                onClick={() => {
-                  navigator.clipboard.writeText(id);
-                }}
-              />
-            </div>
-          );
-        })}
-      </section>
-    </main>
+      {uuids.map((id, index) => {
+        return <UuidRow id={id} key={id} />;
+      })}
+    </>
   );
+
+  // eslint-disable-next-line react/jsx-no-undef
+  return <ToolTemplate metadata={toolMetaData} tool={tool} />;
 };
 
 export default UuidGenerator;
